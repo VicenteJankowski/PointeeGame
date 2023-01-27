02a0263b8a9f91c7ee7341db0f7fc23967705acc
VicenteJankowski <jxstudiopl@gmail.com>
2023-01-28 00:34:26 +0100

    Added contains method

--- src/main/java/org/example/Checkerboard.java
+++ src/main/java/org/example/Checkerboard.java
@@ -1,34 +1,40 @@
 package org.example;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
 public class Checkerboard {
 
-    private int[][] checkerboard = new int[15][15];
+    final private int[][] checkerboard = new int[15][15];
 
     Checkerboard() {
         for (int[] row : checkerboard)
             Arrays.fill(row,1);
     }
 
-    void birdApproachesToField(Point birdPosition) {
+    void birdApproachesToField(final Point birdPosition) {
         List<Point> adjacentSquares = getAdjacentSquaresTo(birdPosition);
-        Point wherePointeesWillEscape = adjacentSquares.get(generateRandomValueInRange(0, adjacentSquares.size() - 1));
+        Point wherePointeesWillEscape = adjacentSquares.get(RandomGenerator.generateFromRange(0, adjacentSquares.size() - 1));
         checkerboard[wherePointeesWillEscape.x][wherePointeesWillEscape.y] += checkerboard[birdPosition.x][birdPosition.y];
         checkerboard[birdPosition.x][birdPosition.y] = 0;
     }
 
-    private List<Point> getAdjacentSquaresTo(Point centralSquare) {
+    public List<Point> getAdjacentSquaresTo(final Point centralSquare) {
         List<Point> adjacentSquares = new ArrayList<>();
 
         for (int i = -1; i <= 1; i++) {
-            if (centralSquare.x + i < 0 || centralSquare.x + i >= checkerboard.length) continue;
+            if (!this.contains(new Point(centralSquare.x + i, 0))) continue;
             for (int j = -1; j <= 1; j++) {
-                if (centralSquare.y + j < 0 || centralSquare.y + j >= checkerboard[0].length) continue;
                 Point nextAdjacentSquare = new Point(centralSquare.x + i, centralSquare.y + j);
+                if (!this.contains(nextAdjacentSquare)) continue;
                 if (!centralSquare.equals(nextAdjacentSquare))
                     adjacentSquares.add(nextAdjacentSquare);
             }
@@ -37,20 +43,28 @@ public class Checkerboard {
         return adjacentSquares;
     }
 
-    private int generateRandomValueInRange(int minRange, int maxRange) {
-        return minRange + (int) Math.round(Math.random() * (maxRange - minRange));
-    }
-
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
 
-        for (int i = 0; i < checkerboard.length; i++) {
+        for (int[] ints : checkerboard) {
             for (int j = 0; j < checkerboard[0].length; j++)
-                result.append(String.format("%03d", checkerboard[i][j]) + "   ");
+                result.append(String.format("%03d", ints[j]) + "   ");
             result.append(System.getProperty("line.separator"));
         }
 
         return result.toString();
+    }
+
+    public boolean contains(@NotNull Point newBirdPosition) {
+        return newBirdPosition.x >= 0 &
+               newBirdPosition.x < checkerboard.length &
+               newBirdPosition.y >= 0 &
+               newBirdPosition.y < checkerboard[0].length
+                ? TRUE : FALSE;
+    }
+
+    public boolean contains(int x, int y) {
+        return contains(new Point(x,y));
     }
 }
