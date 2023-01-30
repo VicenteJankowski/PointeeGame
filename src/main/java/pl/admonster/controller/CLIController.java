f496350f8431d809ad041503c9f1f552c93056f4
VicenteJankowski <jxstudiopl@gmail.com>
2023-01-30 01:00:50 +0100

    ADD: new controller class: CLIController

--- src/main/java/pl/admonster/controller/CLIController.java
+++ src/main/java/pl/admonster/controller/CLIController.java
@@ -1,12 +1,70 @@
 package pl.admonster.controller;
 
+import pl.admonster.model.board.Board;
+import pl.admonster.model.board.Checkerboard;
+import pl.admonster.model.movingObject.Bird;
+import pl.admonster.model.movingObject.MovingObject;
+import pl.admonster.service.Game;
+
 import java.awt.*;
+import java.util.Scanner;
 
 public class CLIController {
 
-    public static Point askUserForNewCoordinates(){
+    private static Game game;
+    public static void start(){
+        System.out.println("Welcome to PointeesGame!");
+        System.out.println("If you want to finish game type non-digit character or string, when prompted to type anything.");
 
-        return null;
+        initializeGame();
+        while (game.isNotfinshed()) {
+            System.out.println("ROUND NUMBER " + game.getRoundNumber());
+            System.out.println("Please select field for Bird to start flying.");
+            game.playRound(askUserForNewCoordinates());
+            System.out.println(game.getGameBoard());
+        }
+        sumUpGame();
+    }
+
+    private static void initializeGame() {
+        Board gameBoard = new Checkerboard();
+        MovingObject movingObject = new Bird();
+        game = new Game(gameBoard, movingObject);
+
+        Point typedPoint;
+        do {
+            System.out.println("Please select field you would like to redeem.");
+            typedPoint = askUserForNewCoordinates();
+        } while (!gameBoard.contains(typedPoint));
+
+        game.setSelectedToRedeem(typedPoint);
+    }
+
+    private static void sumUpGame() {
+    }
+
+    public static Point askUserForNewCoordinates(){
+        Scanner s = new Scanner(System.in);
+        Point typedPoint;
+        System.out.println("Type X coordinate: ");
+        typedPoint = new Point();
+        try {
+            typedPoint.x = Integer.parseInt(s.next());
+        } catch (NumberFormatException e) {
+            gameFinishedByUser();
+        }
+        System.out.println("Type Y coordinate: ");
+        try {
+            typedPoint.y = Integer.parseInt(s.next());
+        } catch (NumberFormatException e) {
+            gameFinishedByUser();
+        }
+        return typedPoint;
+    }
+
+    private static void gameFinishedByUser(){
+        System.out.println("Typed string is not valid number. Game over.");
+        System.exit(0);
     }
 
 }
